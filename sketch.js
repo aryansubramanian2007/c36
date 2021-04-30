be967e50cebdf914d9333cb60d2902334fe85b7f
aryansubramanian2007 <75075845+aryansubramanian2007@users.noreply.github.com>
2021-04-30 17:54:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var gameState=0
+var playerCount
+var form,player,game
+var database
+var position
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+   
+}
+
+function draw(){
+    background("white");
+    
+}
+
